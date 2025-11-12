93392c5b9d4fff544ffd18665c1ef6d95ec291fb
launzoa <carmim_birigui@live.com>
2025-11-12 16:32:42 -0300

    att

---
+++ Grafos/1610/ex1610.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int find() {}
+
+int main() {
+  int t, n, m;
+
+  scanf("%d", &t);
+  for (int i = 0; i < t; i++) {
+    scanf("%d%d", &n, &m);
+
+    for (int j = 0; j < m; j++) {
+    }
+  }
+  return 0;
+}
